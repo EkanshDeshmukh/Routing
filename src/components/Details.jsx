1a5b8c99065c5b65444fe996175d63894e277b2b
EkanshDeshmukh <yashudeshmukh25@gmail.com>
2024-04-25 23:21:18 +0530

    understanded react-router-dom

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { Link, useLocation, useNavigate, useParams } from 'react-router-dom'
 
 const Details = () => {
     const { id } = useParams()
@@ -7,10 +7,13 @@ const Details = () => {
     const submitHandler = () => {
         navigate(-1)
     }
+    const { pathname } = useLocation()
     return (
         <div>
             <h1 className='text-3xl font-semibold mt-5'>Library List</h1>
-            <h1 className='text-xl mt-3'>Book <span className='font-bold '>{id}</span></h1>
+            <h1 className='text-xl mt-3 mb-5'>Book Id : <span className='font-bold '>{id}</span></h1>
+            < Link to={`${pathname}/profile`} className='bg-red-500 px-3 py-2 text-white rounded'>Profile</ Link>
+            <br />
             <button className='px-3 py-2 bg-blue-500 text-white rounded mt-5' onClick={submitHandler}>Go Back</button>
         </div>
     )
