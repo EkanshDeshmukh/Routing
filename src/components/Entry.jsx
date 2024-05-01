c9ff72ca444f13151cea35e05d2093276a67c9a3
EkanshDeshmukh <yashudeshmukh25@gmail.com>
2024-05-01 22:41:00 +0530

    Added React-lifecycle

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,30 +1,43 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from '../utils/axios'
 
 const Entry = () => {
-  const [posts, setposts] = useState([])
+  const [images, setimages] = useState([])
+  const [page, setpage] = useState(1)
+  const navigate = useNavigate()
   const getPosts = async () => {
     try {
-      const { data } = await axios.get('posts')
-      console.log(data);
-      setposts(data)
-
+      const { data } = await axios.get(`https://picsum.photos/v2/list?page=${page}&limit=10`)
+      setimages(data)
     } catch (error) {
       console.log(error);
     }
   }
-  const navigate = useNavigate()
+
   const submitHandler = () => {
     navigate('/Library')
-
   }
+  useEffect(() => {
+    getPosts()
+  }, [page])
   return (
     <div className='mt-5'>
       <h1 className='text-3xl font-bold m-5'>This is EntryPage</h1>
-      <button onClick={getPosts} className='bg-blue-500 m-5 text-white px-3 py-3 rounded'>Get Posts</button> <br />
       <button onClick={submitHandler} className='px-5 py-3 rounded bg-red-300 m-5 text-white'>Submit</button>
-      {posts.map((post, i) => { return <h1 key={i}>{post.id} {post.title}</h1> })}
+      {images.map((image, i) => (
+        <div key={i}>
+          <img src={image.download_url} alt="" />
+          <small>{image.author}</small>
+        </div>
+      ))}
+
+
+      <div>
+        <span onClick={() => setpage(pre => pre - 1)} className='text-blue-500 text-2xl'>Prev</span>
+        <span onClick={() => setpage(pre => pre + 1)} className='text-blue-500 text-2xl'>Next</span>
+
+      </div>
     </div>
   )
 }
